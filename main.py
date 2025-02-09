e3f570ddddf8a5ce898636cca6e5819be363cdc4
nels2248 <nels2248@gmail.com>
2025-02-09 17:33:22 -0600

    chart as index.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pandas as pd
 import regex as re
 from datetime import datetime
 import json
+import plotly.express as px
 
 #create beautiful soup objects
 headers = {
@@ -74,4 +75,8 @@ df.to_csv('mlbfutures.csv', mode='a', index=False, header=False)
 
 #show results in html.  first pull complete csv then display in html
 df_full = pd.read_csv('mlbfutures.csv')
-df_full.to_html('index.html')
+# Create a line chart using plotly
+fig = px.line(df_full, x='dateandtimeran', y='odds', color='team', title='Odds Over Time by Team', markers=True)
+
+# Save the plot as an HTML file
+fig.write_html('index.html')
