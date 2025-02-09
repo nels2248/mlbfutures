cec99c026791792a9a77b508bf79d2183b3d78f1
nels2248 <nels2248@gmail.com>
2025-02-09 17:23:36 -0600

    chart start

---
+++ scrap.py
@@ -0,0 +1,17 @@
+#USED IN CREATING DIFFERENT OUTPUT FILES
+
+import pandas as pd
+import plotly.express as px
+
+#show results in html.  first pull complete csv then display in html
+df_full = pd.read_csv('mlbfutures.csv')
+df_full.to_html('index.html')
+
+# Create a line chart using plotly
+fig = px.line(df_full, x='dateandtimeran', y='odds', color='team', title='Odds Over Time by Team')
+
+# Save the plot as an HTML file
+fig.write_html('odds_over_time_by_team.html')
+
+# Optionally, display the plot
+fig.show()
